c95cff3a0bb01b0db3ad1d8a8070c8ab97124dd0
Eduard Epurica <epurica.eduard@gmail.com>
2023-10-09 21:51:42 +0300

    Initial Commit

--- src/main/java/com/eduard/cruddemo/CruddemoApplication.java
+++ src/main/java/com/eduard/cruddemo/CruddemoApplication.java
@@ -1,13 +1,24 @@
 package com.eduard.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	//This will be executed after the spring beans have been loaded
+	public CommandLineRunner commandLineRunner(String[] args){
+
+		return runnter -> {
+			System.out.println("Hello wrold");
+		};
 	}
 
 }
